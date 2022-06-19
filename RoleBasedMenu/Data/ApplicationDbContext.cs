7c417f84cbc800eb710930a6269877a5a1515ce0
User <User@DESKTOP-45R4NSQ>
2022-06-19 12:00:10 +0600

    Add project files.

---
+++ RoleBasedMenu/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RoleBasedMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoleBasedMenu.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<RoleMenuPermission> RoleMenuPermission { get; set; }
+
+		public DbSet<NavigationMenu> NavigationMenu { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+	}
+}
